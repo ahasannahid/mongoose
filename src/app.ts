97c2dee7befbcae9b793e7c5b36f1b646d6cc885
ahasannahid <ahasan15-9072@diu.edu.bd>
2023-06-19 20:35:01 +0600

    create interface and schema

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,17 @@
 import express, { Application, NextFunction, Request, Response } from 'express'
 import cors from 'cors';
+import { Schema } from 'mongoose';
 
-const app : Application = express();
+const app: Application = express();
 // using cors
 app.use(cors())
 
 
 //parse data
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req : Request, res : Response, next : NextFunction) => {
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
     // res.send('Hello World!');
     // next();
 
@@ -21,6 +22,82 @@ app.get('/', (req : Request, res : Response, next : NextFunction) => {
      * step 3: convert schema to model
      * step 4: create Database query
      */
-})
+
+    // 1. Create an interface representing a document in MongoDB.
+    interface IUser {
+        id: string;
+        role: 'student';
+        password: string;
+        name: {
+            firstName: string;
+            middleName?: string;
+            lastName: string;
+        };
+        dateOfBirth?: string;
+        gender: 'male' | 'female';
+        email?: string;
+        contactNo: string;
+        emergencyContactNo: string;
+        presentAddress: string;
+        permanentAddress: string;
+    }
+    // 2. Create a Schema corresponding to the document interface.(schema er somoy type er first letter capital hobe. like String)
+    const userSchema = new Schema<IUser>({
+        id: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        role: {
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        name: {
+            firstName: {
+                type: String,
+                required: true,
+            },
+            middleName: {
+                type: String,
+            },
+            lastName: {
+                type: String,
+                required: true,
+            }
+        },
+        dateOfBirth: {
+            type: String,
+        },
+        gender: {
+            type: String,
+            enum: ['male' , 'female']
+        },
+        email: {
+            type: String,
+        },
+        contactNo: {
+            type: String,
+            required: true,
+        },
+        emergencyContactNo: {
+            type: String,
+            required: true,
+        },
+        presentAddress: {
+            type: String,
+            required: true,
+        },
+        permanentAddress: {
+            type: String,
+            required: true,
+        }
+    });
+
+});
+
 
 export default app;
